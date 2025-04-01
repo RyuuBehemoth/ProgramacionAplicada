fded480739ac3668baa58960a35106b15c920b23
Kevin Pedraza <130472205+RyuuBehemoth@users.noreply.github.com>
2025-04-01 09:53:39 -0500

    Update LedSequence.py

--- Corte1/Micropython/LedSequence.py
+++ Corte1/Micropython/LedSequence.py
@@ -1,7 +1,9 @@
 import machine
 import utime
+
 led_pins = [15,2,4,5,18,19,21,22]
 leds = [machine.Pin(pin,machine.Pin.OUT) for pin in led_pins]
+
 while True:
     for led in leds:
         led.value(1)

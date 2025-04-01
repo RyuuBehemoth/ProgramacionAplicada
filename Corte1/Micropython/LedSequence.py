266b7977e7c90c87593fa5d712f3bffaf1f884e9
Kevin Pedraza <130472205+RyuuBehemoth@users.noreply.github.com>
2025-04-01 09:22:58 -0500

    Add files via upload

---
+++ Corte1/Micropython/LedSequence.py
@@ -0,0 +1,9 @@
+import machine
+import utime
+led_pins = [15,2,4,5,18,19,21,22]
+leds = [machine.Pin(pin,machine.Pin.OUT) for pin in led_pins]
+while True:
+    for led in leds:
+        led.value(1)
+        utime.sleep(0.5)
+        led.value(0)
